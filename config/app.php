3b999d630bb584ea57ba7662f6098953a6fdc4dd
Bertho <berthojoris@gmail.com>
2020-11-12 17:57:44 +0700

    Revert "Install excel lib" q This reverts commit 5dc157a1514ac74097dfb789962e659bea91b40a.

--- config/app.php
+++ config/app.php
@@ -174,8 +174,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        Barryvdh\Debugbar\ServiceProvider::class,
-        Maatwebsite\Excel\ExcelServiceProvider::class,
+		Barryvdh\Debugbar\ServiceProvider::class,
 
     ],
 
@@ -228,8 +227,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-        'Debugbar' => Barryvdh\Debugbar\Facade::class,
-        'Excel' => Maatwebsite\Excel\Facades\Excel::class,
+		'Debugbar' => Barryvdh\Debugbar\Facade::class,
 
     ],
 
